9582b01f1f9ff8ea314b0e6539bb97f9de58b232
reginaldl <reginald.l@gmail.com>
2013-03-29 15:46:01 -0700

    Improve cosmetic of directory listing.

--- src/proto/http/http_file.c
+++ src/proto/http/http_file.c
@@ -45,49 +45,64 @@ int rinoohttp_send_dir(t_rinoohttp *http, const char *path)
 		     "  <head>\n"
 		     "    <title>Directory listing</title>\n"
 		     "    <style>\n"
-		     "      body { font-family: Monospace; }\n"
-		     "      #dirlist { width: 800px; border: 1px solid #888; padding: 10px; margin: 0 auto; }\n"
-		     "      #dirlist ul { padding: 0px; list-style: none; }\n"
-		     "      #dirlist li { padding: 2px; margin: 0px; }\n"
-		     "      #dirlist li a { display: block; border: 1px dotted #888; padding: 0px 10px; height: 15px; }\n"
-		     "      a { margin: 0px; text-decoration: none; color: #000; }\n"
-		     "      .dl_title { font-size: 18px; color: #888; }\n"
+		     "      body { font-family: Monospace; color: #666; font-size: 14px; }\n"
+		     "      #shadow { width: 800px; border-radius: 100% / 33px; box-shadow: 0 8px 3px -5px rgba(0, 0, 0, .2); margin: 20px auto; }\n"
+		     "      #dirlist { border-radius: 5px; background: -webkit-linear-gradient(top, rgba(249, 249, 249, 1) 50%, rgba(240, 240, 240, 1) 100%); box-shadow: 0 -3px 1px rgba(255, 255, 255, .6), inset 0 1px 1px rgba(255, 255, 255, .97), 0 0 3px 0px rgba(0, 0, 0, .7); padding: 0px; }\n"
+		     "      #dirlist ul { padding: 10px; margin: 0; list-style: none; }\n"
+		     "      #dirlist li { padding: 0; margin: 0; }\n"
+		     "      #dirlist li a { display: block; padding: 2px 10px; height: 15px; border-radius: 5px;  margin: 3px 0; }\n"
+		     "      a { margin: 0px; text-decoration: none; color: #666; }\n"
+		     "      .dl_title { padding: 10px; border-bottom: 1px solid #eee; box-shadow: 0 1px 1px rgba(255, 255, 255, .6); text-shadow: 0 1px 1px rgba(255, 255, 255, .6); font-size: 16px; font-weight: bold; }\n"
 		     "      .dl_en { float: left; width: 500px; }\n"
 		     "      .dl_ed { float: left; width: 130px; text-align: center; }\n"
 		     "      .dl_es { float: left; width: 120px; text-align: right; }\n"
-		     "      .dl_hl { background-color: #eee; }\n"
+		     "      .dl_hl { box-shadow: 0 0px 1px 0 rgba(0, 0, 0, .2); background-color: #fff; }\n"
 		     "    </style>\n"
 		     "  </head>\n"
 		     "  <body>\n"
-		     "    <div id=\"dirlist\">\n"
-		     "      <div class=\"dl_title\">Directory listing</div>\n"
-		     "      <ul>\n");
-	flag = 0;
+		     "    <div id=\"shadow\">\n"
+		     "      <div id=\"dirlist\">\n"
+		     "        <div class=\"dl_title\">Directory listing</div>\n"
+		     "        <ul>\n"
+		     "<li>\n"
+		     "  <a href=\"../\" class=\"dl_hl\">\n"
+		     "    <div class=\"dl_en\">../</div>\n"
+		     "    <div class=\"dl_ed\">-</div>\n"
+		     "    <div class=\"dl_es\">-</div>\n"
+		     "  </a>\n"
+		     "</li>\n");
+	flag = 1;
 	while ((curentry = readdir(dir)) != NULL) {
-		if (flag == 0) {
-			hl = " class=\"dl_hl\"";
-		} else {
-			hl = "";
+		if (strcmp(curentry->d_name, ".") != 0 && strcmp(curentry->d_name, "..") != 0) {
+			if (flag == 0) {
+				hl = " class=\"dl_hl\"";
+			} else {
+				hl = "";
+			}
+			if (curentry->d_type == DT_DIR) {
+				de = "/";
+			} else {
+				de = "";
+			}
+			buffer_print(result,
+				     "<li>\n"
+				     "  <a href=\"%s%s\"%s>\n"
+				     "    <div class=\"dl_en\">%s%s</div>\n"
+				     "    <div class=\"dl_ed\">-</div>\n"
+				     "    <div class=\"dl_es\">-</div>\n"
+				     "  </a>\n"
+				     "</li>\n",
+				     curentry->d_name,
+				     de,
+				     hl,
+				     curentry->d_name,
+				     de);
+			flag = !flag;
 		}
-		if (curentry->d_type == DT_DIR) {
-			de = "/";
-		} else {
-			de = "";
-		}
-		buffer_print(result,
-			     "	<li><a href=\"%s%s\"%s>"
-			     "<div class=\"dl_en\">%s%s</div>"
-			     "<div class=\"dl_ed\">-</div>"
-			     "<div class=\"dl_es\">-</div></a></li>",
-			     curentry->d_name,
-			     de,
-			     hl,
-			     curentry->d_name,
-			     de);
-		flag = ! flag;
 	}
 	buffer_print(result,
-		     "      </ul>\n"
+		     "        </ul>\n"
+		     "      </div>\n"
 		     "    </div>\n"
 		     "  </body>\n"
 		     "</html>\n");
