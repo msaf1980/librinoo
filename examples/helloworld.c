7e333a8d574c6a3aebe542cc470a087c171c8229
reginaldl <reginald.l@gmail.com>
2013-10-19 14:43:42 -0700

    Fix indent

--- examples/helloworld.c
+++ examples/helloworld.c
@@ -2,32 +2,32 @@
 
 void task_client(void *socket)
 {
-    	char a;
+	char a;
 
-    	rinoo_socket_write(socket, "Hello world!\n", 13);
-    	rinoo_socket_read(socket, &a, 1);
-    	rinoo_socket_destroy(socket);
+	rinoo_socket_write(socket, "Hello world!\n", 13);
+	rinoo_socket_read(socket, &a, 1);
+	rinoo_socket_destroy(socket);
 }
 
 void task_server(void *sched)
 {
-    	t_rinoosocket *server;
-    	t_rinoosocket *client;
+	t_rinoosocket *server;
+	t_rinoosocket *client;
 
-    	server = rinoo_tcp_server(sched, 0, 4242);
-    	while ((client = rinoo_tcp_accept(server, NULL, NULL)) != NULL) {
-    		rinoo_task_start(sched, task_client, client);
-    	}
-    	rinoo_socket_destroy(server);
+	server = rinoo_tcp_server(sched, 0, 4242);
+	while ((client = rinoo_tcp_accept(server, NULL, NULL)) != NULL) {
+		rinoo_task_start(sched, task_client, client);
+	}
+	rinoo_socket_destroy(server);
 }
 
 int main()
 {
-    	t_rinoosched *sched;
+	t_rinoosched *sched;
 
-    	sched = rinoo_sched();
-    	rinoo_task_start(sched, task_server, sched);
-    	rinoo_sched_loop(sched);
-    	rinoo_sched_destroy(sched);
-    	return 0;
+	sched = rinoo_sched();
+	rinoo_task_start(sched, task_server, sched);
+	rinoo_sched_loop(sched);
+	rinoo_sched_destroy(sched);
+	return 0;
 }
--- examples/helloworld_multithread.c
+++ examples/helloworld_multithread.c
@@ -2,35 +2,35 @@
 
 void task_client(void *socket)
 {
-    	char a;
+	char a;
 
-    	rinoo_socket_write(socket, "Hello world!\n", 13);
-    	rinoo_socket_read(socket, &a, 1);
-    	rinoo_socket_destroy(socket);
+	rinoo_socket_write(socket, "Hello world!\n", 13);
+	rinoo_socket_read(socket, &a, 1);
+	rinoo_socket_destroy(socket);
 }
 
 void task_server(void *server)
 {
 	t_rinoosched *sched;
-    	t_rinoosocket *client;
+	t_rinoosocket *client;
 
 	sched = rinoo_sched_self();
-    	while ((client = rinoo_tcp_accept(server, NULL, NULL)) != NULL) {
+	while ((client = rinoo_tcp_accept(server, NULL, NULL)) != NULL) {
 		rinoo_log("Accepted connection on thread %d", sched->id);
-    		rinoo_task_start(sched, task_client, client);
-    	}
-    	rinoo_socket_destroy(server);
+		rinoo_task_start(sched, task_client, client);
+	}
+	rinoo_socket_destroy(server);
 }
 
 int main()
 {
 	int i;
-    	t_rinoosched *spawn;
-    	t_rinoosched *sched;
-    	t_rinoosocket *server;
+	t_rinoosched *spawn;
+	t_rinoosched *sched;
+	t_rinoosocket *server;
 
-    	sched = rinoo_sched();
-    	server = rinoo_tcp_server(sched, 0, 4242);
+	sched = rinoo_sched();
+	server = rinoo_tcp_server(sched, 0, 4242);
 	/* Spawning 10 schedulers, each running in a separate thread */
 	rinoo_spawn(sched, 10);
 	for (i = 1; i <= 10; i++) {
@@ -38,7 +38,7 @@ int main()
 		rinoo_task_start(spawn, task_server, rinoo_socket_dup(spawn, server));
 	}
 	rinoo_task_start(sched, task_server, server);
-    	rinoo_sched_loop(sched);
-    	rinoo_sched_destroy(sched);
-    	return 0;
+	rinoo_sched_loop(sched);
+	rinoo_sched_destroy(sched);
+	return 0;
 }
