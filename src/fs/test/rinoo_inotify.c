17a42df3b167b0421fbc35b9941a0285028e550f
reginaldl <reginald.l@gmail.com>
2014-07-17 09:54:42 -0700

    Fix inotify unit test.

--- src/fs/test/rinoo_inotify.c
+++ src/fs/test/rinoo_inotify.c
@@ -10,21 +10,53 @@
 
 #include "rinoo/rinoo.h"
 
+#define NB_EVENT	20
+
+static int nb_create = 0;
+static int nb_create_event = 0;
+static int nb_rm = 0;
+static int nb_rm_event = 0;
+
+void event_generator(void *sched)
+{
+	int i;
+	int fd;
+	char path[100];
+
+	rinoo_task_wait(sched, 200);
+	for (i = 0; i < NB_EVENT / 2; i++) {
+		snprintf(path, sizeof(path), "/tmp/.inotify.XXXXXX");
+		fd = mkstemp(path);
+		close(fd);
+		rinoo_log("Event generator: file created.");
+		nb_create++;
+		rinoo_task_wait(sched, 200);
+		unlink(path);
+		nb_rm++;
+		rinoo_log("Event generator: file removed.");
+		rinoo_task_wait(sched, 200);
+	}
+}
+
 void check_file(void *sched)
 {
+	int i;
 	t_rinoo_inotify *inotify;
 	t_rinoo_inotify_event *event;
 
 	inotify = rinoo_inotify(sched);
 	rinoo_inotify_add_watch(inotify, "/tmp", INOTIFY_CREATE | INOTIFY_DELETE, false);
-	while ((event = rinoo_inotify_event(inotify)) != NULL) {
+	for (i = 0; i < NB_EVENT && (event = rinoo_inotify_event(inotify)) != NULL; i++) {
 		if (event->type & INOTIFY_CREATE) {
 			rinoo_log("File created.");
+			nb_create_event++;
 		} else if (event->type & INOTIFY_DELETE) {
 			rinoo_log("File deleted.");
+			nb_rm_event++;
 		}
 	}
-	rinoo_log("%s", strerror(errno));
+	XTEST(nb_create_event == nb_create);
+	XTEST(nb_rm_event == nb_rm);
 	rinoo_inotify_destroy(inotify);
 }
 
@@ -41,6 +73,7 @@ int main()
 	sched = rinoo_sched();
 	XTEST(sched != NULL);
 	XTEST(rinoo_task_start(sched, check_file, sched) == 0);
+	XTEST(rinoo_task_start(sched, event_generator, sched) == 0);
 	rinoo_sched_loop(sched);
 	rinoo_sched_destroy(sched);
 	XPASS();
